067a0729d6cd7432c143b79a7b03222572666c4c
Lavkush24 <college.studying23@gmail.com>
2025-06-07 17:09:39 +0530

    some changes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,73 @@
+import { PictureFrame } from "./Components/PictureFrame";
+import { NavBar } from "./Components/NavBar";
+
+function App() {
+    return(
+        <>
+            <Collector/>
+        </>
+    )
+}
+
+function Logo() {
+    return (
+        <div 
+            className="
+            z-20
+            fixed top-2 left-2 
+            w-[100px] h-[100px] 
+            rounded-full
+            bg-gray-500
+            bg-[url(silent stories (6).gif)]
+            object-cover
+            "
+        >
+        </div>
+    )
+}
+
+
+function Collector() {
+    return(
+        <div className="bg-[#18181b] p-10">
+            <Logo/>
+            <NavBar />
+            <div className="grid grid-cols-3 justify-evenly">
+                <div className="pl-[100px] flex items-center min-h-screen">
+                    <PictureFrame 
+                        img={"night.jpg"}
+                        watermark={"@ Echo of Frames"} 
+                        title={"silents moments of streets"} 
+                        description={"From the street of the jaipur"}
+                    />
+                </div>
+                <div className="pl-[100px] flex items-center min-h-screen">
+                    <PictureFrame 
+                        img={"night.jpg"}
+                        watermark={"@ Echo of Frames"} 
+                        title={"silents moments of streets"} 
+                        description={"From the street of the jaipur"}
+                    />
+                </div>
+                <div className="pl-[100px] flex items-center min-h-screen">
+                    <PictureFrame 
+                        img={"night.jpg"}
+                        watermark={"@ Echo of Frames"} 
+                        title={"silents moments of streets"} 
+                        description={"From the street of the jaipur"}
+                    />
+                </div>
+                <div className="pl-[100px] flex items-center min-h-screen">
+                    <PictureFrame 
+                        img={"night.jpg"}
+                        watermark={"@ Echo of Frames"} 
+                        title={"silents moments of streets"} 
+                        description={"From the street of the jaipur"}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App;
